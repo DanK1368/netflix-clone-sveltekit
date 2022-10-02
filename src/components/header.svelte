763483a9f46d6a2c2397ad418611769c4411928f
DanK1368 <danielkoett@gmail.com>
2022-10-02 13:38:27 +0200

    added page links

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -22,13 +22,15 @@
 
 <header class={`${scrollYPosition >= 1 ? 'bg-[#141414]' : null}`}>
 	<div class=" flex items-center space-x-2 md:space-x-10 ">
-		<img
-			src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
-			alt=""
-			width={100}
-			height={100}
-			class="object-contain cursor-pointer"
-		/>
+		<a href="/">
+			<img
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
+				alt=""
+				width={100}
+				height={100}
+				class="object-contain cursor-pointer"
+			/>
+		</a>
 
 		{#if $user}
 			<ul class=" hidden space-x-4 md:flex ">
@@ -46,7 +48,7 @@
 			<iconify-icon width={20} height={20} class=" hidden w-6 h-6 sm:inline" icon="charm:search" />
 			<p class=" hidden lg:inline ">Kids</p>
 			<iconify-icon width={20} height={20} class=" h-6 w-6 " icon="bx:bell" />
-			<a href="/account">
+			<a href="/profile">
 				<img class=" rounded cursor-pointer " src={profileSvg} alt="" width={30} />
 			</a>
 			<button class=" flex items-center justify-center " on:click={handleLogout}>
